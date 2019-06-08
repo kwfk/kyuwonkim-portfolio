8bee7f544176eef68171fb3c2e38b1d7d96f8124
kyuwonk <freddy.kim8@gmail.com>
2019-06-08 00:55:30 -0700

    Modify to work in progress

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Kyu Won Kim's Porfolio Website`,
+    description: `Personal website about Kyu Won (Frederick) Kim`,
+    author: `@kyuwonk`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
